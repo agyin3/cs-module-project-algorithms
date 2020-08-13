24c8b0eac8d932b940939cbc28275d606ec07ce3
Buddy Agyin <agyin3@gmail.com>
2020-08-12 22:40:40 -0600

    first attempt solution

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,9 +3,12 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
-    # Your code here
-
-    pass
+    max_sum = []
+    for x in range((len(nums) - k) + 1):
+        check = nums[x: x + k]
+        max_sum.insert(x, max(check))
+    return max_sum
+        
 
 
 if __name__ == '__main__':
